3238bb1aec1b18e7c938f0017bd6a960c48ea0c4
zakaria farih <zakaria.farih@digitaldatarealtime.com>
2025-03-21 09:18:04 +0100

    Enhanced Theme, Fixed file extensions

--- src/userInterface/App.tsx
+++ src/userInterface/App.tsx
@@ -1,45 +1,35 @@
 import { SideNav, BottomNav } from './components/Nav'
 import style from './App.module.scss'
 import { Route, Routes } from 'react-router-dom'
-import { NavigatePersist } from './supports/Persistence'
 import { EditorPage } from './pages/editor'
 import { ExplorerPage } from './pages/explorer'
+import { AboutAppWrapper } from './components/AboutAppWrapper'
 import 'antd/dist/reset.css'
 import { useWindowSize } from 'react-use'
-// import {
-//   AppOutline,
-//   MessageOutline,
-//   UnorderedListOutline,
-//   UserOutline,
-// } from 'antd-mobile-icons'
-
+import { antdThemeConfig } from './theme/antdThemeConfig'
+import { ConfigProvider } from 'antd'
 
 function App() {
   const { width } = useWindowSize()
   const isWideScreen = width > 600
 
   return (
-    <div className={style.container}>
-      {
-        isWideScreen && <SideNav className={style.sideNav}/>
-      }
-      <main className={style.main}>
-        <Routes>
-          <Route path='/' element={<NavigatePersist to='/editor' />} />
-          <Route path='/editor' element={<EditorPage />} />
-          <Route path='/editor/:parentId/:folderId' element={<EditorPage />} />
-          <Route path='/editor/:parentId/:folderId/:fileId' element={<EditorPage />} />
-          <Route path='/explorer/' element={<ExplorerPage />} />
-          <Route path='/explorer/:parentId/:folderId' element={<ExplorerPage />} />
-          {/* <Route path='/search' element={<SearchPage />} /> */}
-          {/* <Route path='/settings' element={<SettingsPage />} /> */}
-        </Routes>
-      </main>
-      {
-        !isWideScreen && <BottomNav className={style.bottomNav}/>
-      }
-      
-    </div>
+    <ConfigProvider theme={antdThemeConfig}>
+      <div className={style.container}>
+        {isWideScreen && <SideNav className={style.sideNav} />}
+        <main className={style.main}>
+          <Routes>
+            <Route path='/' element={<AboutAppWrapper />} />
+            <Route path='/editor' element={<EditorPage />} />
+            <Route path='/editor/:parentId/:folderId' element={<EditorPage />} />
+            <Route path='/editor/:parentId/:folderId/:fileId' element={<EditorPage />} />
+            <Route path='/explorer/' element={<ExplorerPage />} />
+            <Route path='/explorer/:parentId/:folderId' element={<ExplorerPage />} />
+          </Routes>
+        </main>
+        {!isWideScreen && <BottomNav className={style.bottomNav} />}
+      </div>
+    </ConfigProvider>
   )
 }
 
---
+++ src/userInterface/theme/antdThemeConfig.ts
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+import { ConfigProvider } from 'antd'
+
+export const antdThemeConfig = {
+  token: {
+    colorPrimary: '#82AAFF',
+    colorSuccess: '#7FDBCA',
+    colorWarning: '#F78C6C',
+    colorError: '#FF5370',
+    colorTextBase: '#D6DEEB',
+    colorBgBase: '#021627',
+    colorBorder: '#1F406A',
+  },
+}
